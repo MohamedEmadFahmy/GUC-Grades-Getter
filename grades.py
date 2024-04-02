1d56d3773f524ee5e6005c410e04355d76189bad
MohamedEmadFahmy <mohamedemadfahmy0@gmail.com>
2024-04-02 22:17:57 +0200

    fix formatting and variable scope

--- grades.py
+++ grades.py
@@ -6,30 +6,28 @@ import concurrent.futures
 from dotenv import load_dotenv
 import os
 
-
-def fetchGrades():
-
-    def process_option(option, gradesURL, session, fields, i):
-        option_value = option['value']
-        fields['ctl00$ctl00$ContentPlaceHolderright$ContentPlaceHoldercontent$smCrsLst'] = option_value
+def process_option(option, gradesURL, authentication, fields, i):
+    option_value = option['value']
+    fields['ctl00$ctl00$ContentPlaceHolderright$ContentPlaceHoldercontent$smCrsLst'] = option_value
         
-        response = session.post(gradesURL, data=fields)
+    response = requests.post(gradesURL, data=fields, auth=authentication)
     
-        with open(f"gradepage{i}.html", "w") as file:
-            file.write(response.text)
-            print(option.text)
+    with open(f"gradepage{i}.html", "w") as file:
+        file.write(response.text)
+        print(option.text)
+
+
+
+def fetchGrades(username, password):
+
 
     gradesURL = "https://apps.guc.edu.eg/student_ext/Grade/CheckGrade_01.aspx"
-    load_dotenv()
-    username = os.getenv("guc_username")
-    password = os.getenv("guc_password")
+
     dropDownListId = "ContentPlaceHolderright_ContentPlaceHoldercontent_smCrsLst"
-    # viewStateValue = ""
+
 
     authentication = HttpNtlmAuth(username, password)
-    # session = requests.Session()
-    # session.auth = authentication
-    # response = session.get(gradesURL)
+
     response = requests.get(gradesURL, auth=authentication)
     soup = BeautifulSoup(response.text, 'html.parser')
 
@@ -51,41 +49,26 @@ def fetchGrades():
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
         futures = []
-        session = requests.Session()
-        session.auth = authentication
+
         for i, option in enumerate(options):
             if i == 0:
                 continue
-            futures.append(executor.submit(process_option, option, gradesURL, session, fields, i))
+            futures.append(executor.submit(process_option, option, gradesURL, authentication, fields, i))
         
         for future in concurrent.futures.as_completed(futures):
             pass  # You can handle any cleanup or result processing here if needed
 
 
 
-# start_time = time.time()
-# fetchGrades()
+load_dotenv()
+username = os.getenv("guc_username")
+password = os.getenv("guc_password")
 
-# end_time = time.time()
-# elapsed_time = end_time - start_time
-# print("Elapsed time: {:.6f} seconds".format(elapsed_time))
+start_time = time.time()
+fetchGrades(username, password)
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+print("Elapsed time: {:.6f} seconds".format(elapsed_time))
         
 
-# Number of times to run the function
-num_runs = 15
-
-# Measure the total elapsed time
-total_elapsed_time = 0
-for _ in range(num_runs):
-    start_time = time.time()
-    fetchGrades()
-    end_time = time.time()
-    total_elapsed_time += end_time - start_time
-
-# Calculate average elapsed time
-average_elapsed_time = total_elapsed_time / num_runs
-
-print("Average elapsed time for {} runs: {:.6f} seconds".format(num_runs, average_elapsed_time))
-
-
-# 5.94
